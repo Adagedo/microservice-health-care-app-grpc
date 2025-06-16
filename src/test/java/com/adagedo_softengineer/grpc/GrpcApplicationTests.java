8945a95c89899fa099ad680eaff203126108d1c1
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-06-17 00:44:13 +0100

    initial commit

---
+++ src/test/java/com/adagedo_softengineer/grpc/GrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adagedo_softengineer.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
