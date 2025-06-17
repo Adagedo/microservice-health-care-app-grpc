ef90157bf9bcb4442871e581f2a61dd92bab6a3e
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-06-17 02:14:12 +0100

    created the patient repository

--- patient-service/src/main/java/com/adagedo_softengineer/model/Patient.java
+++ patient-service/src/main/java/com/adagedo_softengineer/model/Patient.java
@@ -1,7 +1,26 @@
 package com.adagedo_softengineer.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Patient {
 
+    @Id
+    @GeneratedValue
+    private Integer id
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String phone;
+    private String address;
 }
