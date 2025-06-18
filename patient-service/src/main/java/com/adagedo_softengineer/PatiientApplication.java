8417de1da0cf3a06c45360450d859c9fa2f5b6ad
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-06-18 14:06:10 +0100

    created tge patient application and the application.yml file for db connection

---
+++ patient-service/src/main/java/com/adagedo_softengineer/PatiientApplication.java
@@ -0,0 +1,13 @@
+package com.adagedo_softengineer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatiientApplication {
+    
+    public static void main(String[] args) {
+        
+        SpringApplication.run(PatiientApplication.class, args);
+    }
+}
