7a52981fde27484b7f44a1ff2d1e121af8f6ee16
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-06-18 13:59:15 +0100

    refactor

--- patient-service/src/main/java/com/adagedo_softengineer/service/PatientService.java
+++ patient-service/src/main/java/com/adagedo_softengineer/service/PatientService.java
@@ -13,60 +13,55 @@ import com.adagedo_softengineer.repo.PatientRepository;
 import io.grpc.stub.StreamObserver;
 
 @Service
-public class PatientService extends PatientServiceImplBase{
+public class PatientService extends PatientServiceImplBase {
 
-    // repository injections 
+    // repository injection 
     private final PatientRepository repository;
 
     public PatientService(PatientRepository repository) {
         this.repository = repository;
     }
 
-        // register a new patient 
-     @Override
-     public void registerPatient(PatientRegistrationRequest request,
-             StreamObserver<PatientRegistrationResponse> responseObserver) {
-        
+
+    // register patient 
+    @Override
+    public void registerPatient(PatientRegistrationRequest request,
+            StreamObserver<PatientRegistrationResponse> responseObserver) {
                 Patient patient = new Patient(
                     null, 
-                        request.getFirstName(), 
-                        request.getLastName(), 
-                        request.getEmail(), 
-                        request.getPhone(), 
-                        request.getAddress()
+                    request.getFirstName(), 
+                    request.getLastName(),
+                    request.getEmail(), 
+                    request.getPhone(), 
+                    request.getAddress()
                 );
-                // save patient to database 
+
                 repository.save(patient);
-
-                // return a response to the grpc server 
-
                 responseObserver.onNext(PatientRegistrationResponse.newBuilder().setPatientId(patient.getId()).build());
                 responseObserver.onCompleted();
-        }
+    }
+
+
     @Override
-    public void getPatientDetails(GetPatientDetailRequest request,
-            StreamObserver<GetPatientDetailsResponse> responseObserver) {
+    public void getPatientDetails(GetPatientDetailRequest request, StreamObserver<GetPatientDetailsResponse> responseObserver) {
+
         var patient = repository.findById(request.getPatientId());
         if (patient.isPresent()) {
             var p = patient.get();
             responseObserver.onNext(GetPatientDetailsResponse.newBuilder()
-                .setPatientId(p.getId())
                 .setFirstName(p.getFirst_name())
                 .setLastName(p.getLast_name())
-                .setEmail(p.getEmail())
+                .setPatientId(p.getId())
                 .setPhone(p.getPhone())
                 .setAddress(p.getAddress())
-                .build()
+                .build()    
             );
         } else {
             responseObserver
                     .onError(io.grpc.Status.NOT_FOUND.withDescription("patient not found").asRuntimeException());
         }
-        
         responseObserver.onCompleted();
+            
     }
-
-
-
     
 }
