3ae076d69344e8b00774ea6cf1b34368d5394602
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-06-19 13:02:57 +0100

    client streaming logic implementation

--- patient-service/src/main/java/com/adagedo_softengineer/service/PatientService.java
+++ patient-service/src/main/java/com/adagedo_softengineer/service/PatientService.java
@@ -3,6 +3,7 @@ package com.adagedo_softengineer.service;
 import org.springframework.stereotype.Service;
 
 import com.adagedo_softengineer.model.Patient;
+import com.adagedo_softengineer.patient.Empty;
 import com.adagedo_softengineer.patient.GetPatientDetailRequest;
 import com.adagedo_softengineer.patient.GetPatientDetailsResponse;
 import com.adagedo_softengineer.patient.PatientRegistrationRequest;
@@ -43,25 +44,64 @@ public class PatientService extends PatientServiceImplBase {
 
 
     @Override
-    public void getPatientDetails(GetPatientDetailRequest request, StreamObserver<GetPatientDetailsResponse> responseObserver) {
+    public void getPatientDetails(GetPatientDetailRequest request,
+            StreamObserver<GetPatientDetailsResponse> responseObserver) {
 
         var patient = repository.findById(request.getPatientId());
         if (patient.isPresent()) {
             var p = patient.get();
             responseObserver.onNext(GetPatientDetailsResponse.newBuilder()
-                .setFirstName(p.getFirst_name())
-                .setLastName(p.getLast_name())
-                .setPatientId(p.getId())
-                .setPhone(p.getPhone())
-                .setAddress(p.getAddress())
-                .build()    
-            );
+                    .setFirstName(p.getFirst_name())
+                    .setLastName(p.getLast_name())
+                    .setPatientId(p.getId())
+                    .setPhone(p.getPhone())
+                    .setAddress(p.getAddress())
+                    .build());
         } else {
             responseObserver
                     .onError(io.grpc.Status.NOT_FOUND.withDescription("patient not found").asRuntimeException());
         }
         responseObserver.onCompleted();
-            
+
+    }
+
+
+    // sending stream of data to the erver
+    // client side streaming
+    @Override
+    public StreamObserver<PatientRegistrationRequest> streamPatient(StreamObserver<Empty> responseObserver) {
+        
+        // return new server stream
+
+        return new StreamObserver<PatientRegistrationRequest>(){
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+                
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                responseObserver.onError(t);
+                
+            }
+
+            @Override
+            public void onNext(PatientRegistrationRequest request) {
+                Patient patient = new Patient(
+                    null, 
+                    request.getFirstName(),
+                    request.getLastName(), 
+                    request.getEmail(),
+                    request.getPhone(), 
+                    request.getAddress()
+                );
+                repository.save(patient);
+                
+            }
+
+        };
     }
     
 }
